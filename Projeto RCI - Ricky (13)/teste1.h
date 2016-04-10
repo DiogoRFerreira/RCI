07d79d4929fb6c650fb8868f47f9569f3fab6222
DiogoRFerreira <diogo.r.ferreira@ist.utl.pt>
2016-04-10 14:36:09 +0100

    Remove

--- Projeto RCI - Ricky (13)/teste1.h
+++
@@ -1,8 +0,0 @@
-#ifndef teste1
-#define teste1
-#include "struct_no.h"
-
-void select_tcp(int flag, int desc, int flag_predi, int flag_succi, int tcp_port, char * ip2, int tcp_port1, struct info_no * no, char * udp_ip, int udp_port);
-int unreg(char * ip, int boot_port, int tcp_port, struct info_no * no, int succi_index, int predi_index);
-#endif
-
--- Projeto RCI - Tomas (14)/clienttcp.h
+++
@@ -1,5 +0,0 @@
-#include "clientudp.h"
-
-
-void servidortcp(no meuno, char* nosucc, int flagarq, udpserv udps);
-void pesquisaID(struct in_addr ip, int portonoarq, no meuno, udpserv udps);
